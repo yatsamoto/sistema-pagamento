52fdfd383ff2f7eed15d295390aabf55efb7c6ce
yatsamoto <diegorsilva95@gmail.com>
2024-09-15 23:03:55 -0300

    importação para o postman

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from BD.database import db
 from db_models.payment import Payment
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -10,7 +11,17 @@ db.init_app(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
-    return jsonify({"message": "The payment has been created"})
+    data = request.get_json()
+
+    if 'value' not in data:
+        return jsonify({"message": "Valor Inválido"}), 400
+    
+    expiration_date = datetime.now() + timedelta(minutes=30)
+    new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify({"message": "The payment has been created", "payment": new_payment.to_dict()})
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
