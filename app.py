4129bf0e58da91c04a47be64d448f9f5dcd12a2c
yatsamoto <diegorsilva95@gmail.com>
2024-09-22 03:20:50 -0300

    importação do socketio

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from BD.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
 from payments.pix import Pix
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
@@ -44,5 +46,11 @@ def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
     
 
+# seção de websockets
+
+@socketio.on('connect')
+def hande_connect():
+    print("Cliente connected to the server")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
